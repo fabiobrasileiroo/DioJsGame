b79f0e68e840144fe3a2a9618517361dc93dbc6a
fabiobrasileiroo <fabio.h591@gmail.com>
2024-01-17 12:22:40 -0400

    estudos

---
+++ memory-game/src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+  "🐧",
+  "🐧",
+  "🌎",
+  "🌎",
+  "❄",
+  "❄️",
+  "🧊",
+  "🧊",
+  "🥶",
+  "🥶",
+  "🚢",
+  "🚢",
+  "🤖",
+  "🤖",
+  "🧑‍💻",
+  "🧑‍💻",
+]
+let openCards = []
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2: -1));
+
+for(let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div")
+  box.className  = "item"
+  box.innerHTML = shuffleEmojis[i]
+  document.querySelector(".game")
+}
